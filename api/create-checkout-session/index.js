8e11a9738c3c5530e5493ac0c09237dc6ca8514c
Marshall <benson.marshall@gmail.com>
2025-12-29 11:57:27 -0600

    hid secret key on application settings

--- api/create-checkout-session/index.js
+++ api/create-checkout-session/index.js
@@ -1,4 +1,7 @@
 const Stripe = require('stripe');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async function (context, req) {
